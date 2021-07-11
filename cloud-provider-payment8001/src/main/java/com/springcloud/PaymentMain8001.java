6780dc75a1f61f926e22a72e71dd1d22c0d8dac9
904316429@qq.com <904316429@qq.com>
2021-07-11 16:15:07 +0800

    springcloud 学习篇， 尚学堂17节课程 Eureka client

--- cloud-provider-payment8001/src/main/java/com/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/springcloud/PaymentMain8001.java
@@ -2,14 +2,15 @@ package com.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @description:
  * @author: Jiang
  * @create: 2021-07-10 19:28
  */
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
 
     public static void main(String[] args) {
