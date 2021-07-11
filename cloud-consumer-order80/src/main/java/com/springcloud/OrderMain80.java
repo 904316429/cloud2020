07ee5753773f6f638bdf390493d9a1598e144daf
904316429@qq.com <904316429@qq.com>
2021-07-11 16:19:16 +0800

    springcloud 学习篇， 尚学堂18节课程 Eureka client

--- cloud-consumer-order80/src/main/java/com/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/springcloud/OrderMain80.java
@@ -2,6 +2,7 @@ package com.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @description:
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create: 2021-07-10 20:58
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
 
     public static void main(String[] args) {
